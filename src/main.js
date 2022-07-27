2dd7ac3c90632bfd0d8f3fc3cc0f7543a9154650
Knn <w19951129326@yahoo.com.tw>
2022-07-27 20:23:56 +0800

    建立環境

--- src/main.js
+++ src/main.js
@@ -5,9 +5,3 @@ import App from './App.vue'
 
 createApp(App).mount('#app')
 import "bootstrap/dist/js/bootstrap.js"
-
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue3-bootstrap5-test/'
-    : '/'
-}
