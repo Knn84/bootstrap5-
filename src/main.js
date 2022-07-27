1a5985d7e77d5e945edb6ff2079592f83f9408bd
Knn <w19951129326@yahoo.com.tw>
2022-07-27 19:54:37 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import "bootstrap/dist/css/bootstrap.css"
 import { createApp } from 'vue'
 import App from './App.vue'
 
+
 createApp(App).mount('#app')
 import "bootstrap/dist/js/bootstrap.js"
+
+
+module.exports = {
+    publicPath: '/vue3-bootstrap5-test/'
+}
